61dc3fcb3fd864dccfa19b1225fd4bebd40ab70e
dieend1@gmail.com <dieend1@gmail.com@88d9ccb5-13cd-afb7-92b0-0a47c0cce1aa>
2011-03-06 16:35:23 +0000

    nambah comment. haha. *ga mau kalah

--- src/area.h
+++ src/area.h
@@ -3,14 +3,36 @@
 
 
 class Area {
+private:
+	Grid grid[10][10];
+	/*
+		Merupakan array dari Grid yang berukuran 10x10.
+	*/
+	
 public:
-	int grid[10][10]; // ini harusnya class Grid
 	Area(int& tipe);
-	Area(const Area&);
+	/*
+		Constructor Area dengan menginstansiasi typeArea dengan tipe.
+		0 rumah, 1 lahan, 2 toko
+	*/
+	
 	Area();
-	~Area();
-	Area& operator=(const Area&);
-	static Area factory(int);
-	int typeArea;
+	/*
+		Default ctor. typeArea = 0
+	*/
+	virtual ~Area();
+	/*
+		virtual dtor
+	*/
+	Area& operator=(const Area&); // operator=
+
+	const int typeArea;
+	/*
+		menandakan jenis Area;
+	*/
+	Grid* getGrid(Point);
+	/*
+		mengembalikan address grid yang diacu oleh Point
+	*/
 };
 #endif
