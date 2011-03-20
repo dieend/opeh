f2523d6f2187219fa736c286906d5705983075fa
13509045@std.stei.itb.ac.id <13509045@std.stei.itb.ac.id@88d9ccb5-13cd-afb7-92b0-0a47c0cce1aa>
2011-03-20 03:16:28 +0000

    driver dwarf

---
+++ mdwarf.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <iostream>
+#include "dwarf.h"
+#include "map.h"
+#include "point.h"
+#include <deque>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    Dwarf d0(0),d1(1),d2(2);
+    d1.sleep();
+    d2.sleep();
+    deque<Point> dqp;
+//    d0.bfsdwarf();
+    char c;
+    do
+    {
+      cout << "Press enter to do some job : ";
+      c=getch();
+      if ( (int)c ==13 )
+      {
+        system("cls");
+        Dwarf::nextMove(d0,d1,d2);
+      }
+    }while ((int)c==13);
+    system("PAUSE");
+    return 0;
+}
