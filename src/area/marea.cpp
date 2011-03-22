b273cf320deb4ebb5945689601ae83d2bf3e8dfe
dieend1@gmail.com <dieend1@gmail.com@88d9ccb5-13cd-afb7-92b0-0a47c0cce1aa>
2011-03-22 05:57:41 +0000

    marea dan lain2

---
+++ src/area/marea.cpp
@@ -0,0 +1,13 @@
+#include "area.h"
+
+int main(){
+	Area rumah(0);
+	Area lahan(1);
+	Area toko(2);
+	cout << &rumah;
+	system("pause");
+	cout << &lahan;
+	system("pause");
+	cout << &toko;
+	system("pause");
+}
